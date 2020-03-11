1a2a5cfe0481e920568c5ffb739de82c76aaef1c
alex1092 <alittle1092@gmail.com>
2020-03-11 14:14:36 +1000

    Added a test file

--- tests/controller_test.rb
+++ tests/controller_test.rb
@@ -1,1 +1,21 @@
 #this is my test file`
+
+
+require 'test/unit'
+require_relative '../src/classes.rb'
+
+
+class EmployeeTest < Test::Unit::TestCase
+    
+    #HERE I AM TESTING IF INITIALIZE RETURNS 3 SEPEREATE ARRAYS
+    def test_initialize
+        assert_equal([],[],[])
+    end
+
+    #HERE I AM TESTING IF DATA RETURNS 2 ARRAYS WITH NAME AND STAT DATA
+    def test_data(name, stat)
+        employee.data(["Name"], [10])
+        assert_equal(["Name"] [10], employee.data)
+    end
+end
+employee = Employee.new
