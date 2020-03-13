539abf700593c79567a7d1e8faf6f616609cc60e
alex1092 <alittle1092@gmail.com>
2020-03-13 17:30:28 +1000

    Made some final changes

--- src/controller.rb
+++ src/controller.rb
@@ -1,6 +1,5 @@
 #THESE ARE THE REQUIRED LOCAL FILES
 require_relative "./classes/employee.rb"
 require_relative "./methods/methods.rb"
-require_relative "./bin/test.thor"
 #THIS METHOD RUNS THE APP....... :) Have fun
 run_app
